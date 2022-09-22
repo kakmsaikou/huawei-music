d3915053d6c307442e7d484bf8c3df6b4936744a
TacitusPie <kakmsaikou@gmail.com>
2022-09-22 18:08:38 +0800

    init

---
+++ src/javascript/index.js
@@ -0,0 +1,29 @@
+import './icons.js'
+
+class Player {
+  constructor(node) {
+    this.root = typeof node === 'string' ? document.querySelector(node) : node
+    this.$ = selector => this.root.querySelector(selector)
+    this.$$ = selector => this.root.querySelectorAll(selector)
+    this.songList = []
+    this.currentIndex = 0
+    this.audio = new Audio()
+
+    this.start()
+  }
+
+  start() {
+    fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
+  bind() {
+  }
+}
+
+
+window.p = new Player('#player')
+
